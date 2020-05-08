47353f2dfc1c81b402987896fe40e1a7c939b1ce
Aiedail13 <50230465+Aiedail13@users.noreply.github.com>
2020-05-08 15:28:14 +0700

    Add files via upload

---
+++ factor.c
@@ -0,0 +1,100 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "fs.h"
+
+	
+void opsi(char *arg){
+	   int flag=0;
+
+	   
+	   if(strcmp(arg, "--help")==0){
+		flag=1;
+	   }
+	   else if(strcmp(arg,"--version")==0){
+		flag=2;
+	   }
+	    
+	    
+	
+	    if(flag==1){
+		printf(1,"factor : Perintah ini digunakan untuk menghasilkan faktor dari sebuah integer\n");
+	    printf(1,"factor [NUMBER]\n");
+		printf(1,"OPSI:\n");
+		printf(1,"\t--help : Memperlihatkan bantuan\n");
+		printf(1, "\t --version : Menunjukan informasi tentang versi\n");
+	        exit();
+	    }
+	    else if(flag==2){
+	        printf(1,"factor version 0.01\n");
+		printf(1,"Dibuat oleh Sisop20-F05\n");
+	        exit();
+	    }
+	    else if(flag==0){
+	    printf(1,"factor: read error\n",arg);
+	    exit();
+	    }	
+}
+
+int sqrt(int n){
+	double tmp = n/2;
+	double tmp2=0;
+	
+	while(tmp!=tmp2){
+		tmp2=tmp;
+		tmp=(n/tmp2 + tmp2)/2;	
+	}
+	return tmp;
+}
+	
+int main(int argc,char *argv[]){
+	
+	    int n=0;
+	   
+	
+	for(int i=1;i<argc;i++){
+	    if(argv[i][0]=='-'){
+	        if(argv[i][1]=='-'){
+			opsi(argv[i]);
+		}
+	        else {
+	            printf(1,"factor: read error\n");
+	        }
+	    }
+	}
+	
+	for(int i=1;i<argc;i++){
+	    n = atoi(argv[i]);
+	    if(n==0 && strcmp(argv[i],"0") ){
+	        printf(1,"factor: read error\n",argv[i]);
+	    }
+	    else {
+			printf(1,"%d:",n);
+			if(n==1 || n==0){
+				printf(1,"\n");
+				exit();
+			}
+			while(n%2==0){
+				printf(1,"2 ");
+				n = n/2;
+			}
+		
+			for(int i=3;i<=sqrt(n);i=i+2){
+				while(n%i==0){
+					printf(1,"%d ",i);
+					n = n/i;
+				}
+			}
+		
+			if(n>2){
+				printf(1,"%d ",n);		
+			}
+			printf(1,"\n");
+		}
+	}
+	exit();
+}
+	
+	
+
